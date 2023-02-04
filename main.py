d9c361f763d5871ae4227db47e2d28429c82773f
hereIsRakib <hossain.rakib8011@gmail.com>
2023-02-04 20:49:56 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+first_name,last_name,r_f_name,r_l_name='','','',''
+
+first_name=input("Please enter first name:  ")
+last_name=input("Please enter last name:  ")
+
+r_f_name=first_name[::-1]
+r_l_name=last_name[::-1]
+
+
+print(f'Your reversed name is: {r_l_name}  {r_f_name}')
+
+
+
+
+
+
+
